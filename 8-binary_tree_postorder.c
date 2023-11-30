45ef35d8afbed4e3081dba62e8beecf33fbc2cc8
Prila1 <yarteypriscy@gmail.com>
2023-11-30 01:49:52 -0800

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,8 +1,5 @@
 #include "binary_trees.h"
-<<<<<<< HEAD
-=======
 #include <stdlib.h>
->>>>>>> 3fbf57d8426230b8f883e6de298a3344e3dab3f3
 /**
  * binary_tree_preorder - print elements of tree
  * using pre-order traversal
@@ -10,7 +7,6 @@
  * @func: function to use
  * Return: Nothing
  */
-<<<<<<< HEAD
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
@@ -22,8 +18,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 		func(tree->n);
-	}
-=======
+}
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
@@ -32,5 +27,4 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
->>>>>>> 3fbf57d8426230b8f883e6de298a3344e3dab3f3
 }
